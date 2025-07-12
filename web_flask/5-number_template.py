159d70aa6c4858f44fc8831d5ba0119bfafc490a
Emmy042 <Emmybankzz7@gmail.com>
2025-07-12 16:06:43 +0100

    5. Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for, flash, redirect
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "<p>Hello HBNB</p>"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Python_text(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
